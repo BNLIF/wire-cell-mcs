97847de5b634f7f2bcb9bbbb915365df0e93c3c5
Xin Qian <last.qx@gmail.com>
2018-11-01 10:23:00 -0400

    improve the class

--- inc/WireCellMCSSim/MCSSim.h
+++ inc/WireCellMCSSim/MCSSim.h
@@ -15,12 +15,25 @@ namespace WireCell {
     std::vector<double> y;
     std::vector<double> z;
     
+    std::vector<double> vx; // momentum direction
+    std::vector<double> vy; // momentum direction
+    std::vector<double> vz; // momentum direction
+    std::vector<double> p; // momentum
+    std::vector<double> Q; // charge deposition
+
+    double mass; // mass of the particle
+    double charge; // charge of the particle 
+    
     void clear(){
       N = 0;
-      x.clear(); y.clear(); z.clear();
+      x.clear();
+      y.clear();
+      z.clear();
     }
   };
 
+  
+  
   void RotateUz(TVector3& direction, TVector3& v1);
 
   void MultiScattSim(MCStrack& atrack, int N, std::vector<double> initpos, std::vector<double> initdir);
