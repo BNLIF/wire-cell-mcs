89f5b3717069444f23d119d39d54c96d9e47d326
Xin Qian <last.qx@gmail.com>
2018-11-26 11:08:42 -0500

    improve the application

--- apps/wire-cell-mcs-sim.cxx
+++ apps/wire-cell-mcs-sim.cxx
@@ -21,19 +21,63 @@ using namespace MCS;
 // c.f. http://pdg.lbl.gov/2018/reviews/rpp2018-rev-passage-particles-matter.pdf
 
 
-int main(){
+int main(int argc, char* argv[]){
+  if (argc < 2){
+    std::cerr << "wire-cell-uboone -p[particle type] -t[kinetic energy] -x[x-pos] -y[y-pos] -z[z-pos] -a[x-dir] -b[y dir] -c[z dir] -s[step size]" << std::endl;
+    std::cerr << "particle type:  1 pi, 2 kaon, 3 muon, 4 proton, 5 electron" << std::endl;
+    std::cerr << "kinetic energy in MeV" << std::endl;
+    std::cerr << "position in cm" << std::endl;
+    std::cerr << "step size in cm" << std::endl;
+    
+    return 1;
+  }
+  
   MCStrack atrack;
 
   // particle type ...
   // 1: pi, 2 kaon, 3 muon, 4 proton 5, electron
-  int particle_type = 5;
-
+  int particle_type = 4;
   double T_init = 100*units::MeV; // initial kinetic energy
   TVector3 pos_init(1.5*units::m,30*units::cm,6*units::m);
   TVector3 dir_init(0.4,0.4,0.4);
+  double step_size = 0.1*units::cm;
+  
+  for(Int_t i = 1; i != argc; i++){
+    switch(argv[i][1]){
+    case 'p':
+      particle_type = atoi(&argv[i][2]); 
+      break;
+    case 't':
+      T_init = atof(&argv[i][2])*units::MeV; 
+      break;
+    case 'x':
+      pos_init.SetX(atof(&argv[i][2])*units::cm); 
+      break;
+    case 'y':
+      pos_init.SetY(atof(&argv[i][2])*units::cm);
+      break;
+    case 'z':
+      pos_init.SetZ(atof(&argv[i][2])*units::cm);
+      break;
+    case 'a':
+      dir_init.SetX(atof(&argv[i][2]));
+      break;
+    case 'b':
+      dir_init.SetY(atof(&argv[i][2]));
+      break;
+    case 'c':
+      dir_init.SetZ(atof(&argv[i][2]));
+      break;
+    case 's':
+      step_size = atof(&argv[i][2])*units::cm;
+    }
+  }
+  
+
+  
   dir_init *= 1./dir_init.Mag();
 
-  double step_size = 0.1*units::cm;
+  
 
   MCSTrackSim(atrack, particle_type, T_init, pos_init, dir_init, step_size);
   //LineToyTrackSim(atrack,  pos_init, dir_init, 30, step_size, 5000);
