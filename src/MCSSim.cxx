9bb6ae5a715f7556876c769f34c0364f878c04e6
Xin Qian <last.qx@gmail.com>
2018-12-07 00:16:26 -0500

    add fudge factor

--- src/MCSSim.cxx
+++ src/MCSSim.cxx
@@ -45,6 +45,8 @@ void WireCell::MCS::LineTrackSim(MCStrack& atrack, int particle_type, double T_i
   // proper normalize ... 
   atrack.N = 0;
   dir_init *= 1./dir_init.Mag();
+
+  double fudge_factor = 1.391;
   
   while (T_init >0){
     atrack.x.push_back(pos_init.X());
@@ -58,10 +60,11 @@ void WireCell::MCS::LineTrackSim(MCStrack& atrack, int particle_type, double T_i
     atrack.p.push_back(cal_loss.get_mom(T_init));
     // get charge ...
     double charge = 0;
+
     
     double dEdx = cal_loss.get_dEdx(T_init, step_size); // MeV
     T_init -= dEdx * step_size;
-    charge = dEdx * step_size / (23.6 * units::eV) * lar.recombine_Birks(dEdx,88*units::kelvin,0.273*units::kilovolt/units::cm);
+    charge = dEdx * step_size / (23.6 * units::eV) * lar.recombine_Birks(dEdx*fudge_factor,88*units::kelvin,0.273*units::kilovolt/units::cm);
     // std::cout << T_init/units::MeV << " " << dEdx / (units::MeV/units::cm) << " " << lar.recombine_Birks(dEdx,88*units::kelvin,0.273*units::kilovolt/units::cm) << " " << charge << std::endl;
     atrack.Q.push_back(charge);
     pos_init.SetXYZ(pos_init.X() - dir_init.X() * step_size,
@@ -82,6 +85,8 @@ void WireCell::MCS::MCSTrackSim(MCStrack& atrack, int particle_type, double T_in
   atrack.N = 0;
   dir_init *= 1./dir_init.Mag();
 
+  double fudge_factor = 1.391;
+  
    while (T_init >0){
     atrack.x.push_back(pos_init.X());
     atrack.y.push_back(pos_init.Y());
@@ -99,7 +104,7 @@ void WireCell::MCS::MCSTrackSim(MCStrack& atrack, int particle_type, double T_in
 
     
     
-    charge = dEdx * step_size / (23.6 * units::eV) * lar.recombine_Birks(dEdx,88*units::kelvin,0.273*units::kilovolt/units::cm);
+    charge = dEdx * step_size / (23.6 * units::eV) * lar.recombine_Birks(dEdx*fudge_factor,88*units::kelvin,0.273*units::kilovolt/units::cm);
 
     // std::cout << "haha: " << dEdx / (units::MeV/units::cm) << " " << charge << std::endl;
     // std::cout << lar.recombine_Birks(1.8*units::MeV/units::cm,88*units::kelvin,0.273*units::kilovolt/units::cm);
